9d99211f755eacbce58aa850ec27eb344567b7fd
Iryna Masniuk <rodzynia@gmail.com>
2020-01-05 21:11:38 +0100

    React bootstrap added for UserProfile

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -5,10 +5,11 @@ import faker from 'faker';
 import axios from 'axios';
 import config from './config';
 import Prompt from './components/Prompt';
+import { Container, Row, Col, Navbar, Nav, ListGroup, Button  } from 'react-bootstrap';
 
 class Profile extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             isModified: false,
             canEdit: false,
@@ -101,36 +102,56 @@ class Profile extends React.Component {
     render() {
         //  getUserProfile('au8ujhikect@gmail');
         return (
-            <div className="ui container profile">
-                <div className="profile">
-                    <div className="profile top">
-                        <a href="/" className="avatar">
-                            <img alt="avatar" src={ this.state.profilePicture } />
-                        </a>
-                        <h1 className="name-header">{this.state.username}</h1>
-                    </div>
-                    <div className="button-area">
-                        <div><Link to="/friends">Friends</Link></div>
-                        <div><Link to="/posts">Posts</Link></div>
-                        <div><Link to="/comments">Comments</Link></div>
-                        <div><Link to="/events">Events</Link></div>
-                        <div><Link to="/photos">Photos</Link></div>
-
-                    </div>
-                    <div className="content">
-                        {this.state.isEditing ?
-                            <this.editUser /> :
-                            <this.showUser />}
-                    </div>
+            <Container style={{ background: '#fff' }}>
+                <Navbar bg="light" variant="light">
+                    <Navbar.Brand href="#home">{this.state.username}</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/friends">Home</Nav.Link>
+                        <Nav.Link href="/posts">Posts</Nav.Link>
+                        <Nav.Link href="/events">Events</Nav.Link>
+                    </Nav>
+                </Navbar>
+                <Row bg="light">
+                    <Col sm={2} bg="light">
+                        <div className="left-column">
+                            <a href="/" className="avatar" style={{ margin: '25px', display: 'block' }}>
+                                <img
+                                    alt="avatar"
+                                    width='128px'
+                                    height='128px'
+                                    style={{ borderRadius: '50%' }}
+                                    src={ this.state.profilePicture }
+                                />
+                            </a>
+                            {!this.state.isEditing && (
+                                <Button
+                                    disabled={!this.state.canEdit}
+                                    onClick={this.edit}
+                                    style={{ marginBottom: '20px' }}
+                                >
+                                    Edit Profile
+                                </Button>
+                            )}
+                            <Button
+                                disabled={!this.state.canEdit}
+                                onClick={ this.deleteUser }
+                                style={{ marginBottom: '20px' }}
+                                variant="danger"
+                            >
+                                Delete Profile
+                            </Button>
+                        </div>
+                    </Col>
+                    <Col sm={10}>
+                        {this.state.isEditing
+                            ? <this.editUser />
+                            : <this.showUser />
+                        }
+                    </Col>
+                </Row>
+                <div className='error'>
+                    {this.state?.lastError && `Error during last operation: ${ this.state.lastError }`}
                 </div>
-                <div>
-                    {!this.state.isEditing && <button disabled={!this.state.canEdit} onClick={this.edit}>Edit Profile</button>}
-                    {this.state.isModified
-                        && [<button onClick={this.save}>Save Changes</button>,
-                        <button onClick={this.cancel}>Cancel Changes</button>]}
-                    <button disabled={!this.state.canEdit} onClick={ this.deleteUser }>Delete User Profile</button>
-                </div>
-                <div className='error'>{this.state?.lastError && `Error during last operation: ${ this.state.lastError }`}</div>
                 <Prompt
                     show={this.state.showDeletePrompt}
                     title="confirm user deletion"
@@ -138,60 +159,80 @@ class Profile extends React.Component {
                     onClose={() => this.setState({ showDeletePrompt: false })}
                     onSubmit={() => this.sureDelete(this.state.userId)}
                 />
-            </div>
+            </Container>
         )
     }
 
     editUser = () => {
-        return (<div>
-            <div className="label email">E-mail: {this.state.email}</div>
-            <div className="form-row">
-                <div className="col-md-4">
-                    <label className="birth-date" for="birthday">Birth date: <input type="text" id="birthday"
-                        onChange={e => this.change('dateOfBirth', e)} value={this.state.dateOfBirth} required />
-                    </label>
+        return (
+            <div>
+                <div className="label email" style={{ padding: '10px 0' }}>
+                    E-mail: <a href='mailto:{this.state.email}'>{this.state.email}</a>
                 </div>
-                <div className="col-md-4">
-                    <label className="label phone">Phone: <input type="text" id="phone"
-                        onChange={e => this.change('phoneNumber', e)} value={this.state.phoneNumber} required />
-                    </label>
+                <div className="form-row">
+                    <div className="col-md-4">
+                        <label className="birth-date" for="birthday">Birth date: <input type="text" id="birthday"
+                            onChange={e => this.change('dateOfBirth', e)} value={this.state.dateOfBirth} required />
+                        </label>
+                    </div>
+                    <div className="col-md-4">
+                        <label className="label phone">Phone: <input type="text" id="phone"
+                            onChange={e => this.change('phoneNumber', e)} value={this.state.phoneNumber} required />
+                        </label>
+                    </div>
+                </div>
+                <div className="label relations">Relations:<input type="text" className="form-control" id="relationship"
+                    onChange={e => this.change('relationship', e)} value={this.state.relationship} required />
+                </div>
+                <div className="label hobbies">Hobbies:<input type="text" className="form-control" id="hobbies"
+                    onChange={e => this.change('hobbies', e)} value={this.state.hobbies} required />
+                </div>
+                <div className="label origin-city">City of origin:<input type="text" className="form-control" id="hobbies"
+                    onChange={e => this.change('cityOfOrigin', e)} value={this.state.cityOfOrigin} required />
+                </div>
+                <div className="label city-of-living">Currently live in:<input type="text" className="form-control" id="hobbies"
+                    onChange={e => this.change('cityOfLiving', e)} value={this.state.cityOfLiving} required />
+                </div>
+                <div className="label work-place">Work place:<input type="text" className="form-control" id="hobbies"
+                    onChange={e => this.change('work', e)} value={this.state.work} required />
+                </div>
+                <div className="label education">Education:<input type="text" className="form-control" id="hobbies"
+                    onChange={e => this.change('education', e)} value={this.state.education} required />
+                </div>
+                
+                <div style={{ margin: '20px 0'}}>
+                    <Button
+                        disabled={!this.state.isModified}
+                        onClick={this.save}
+                        style={{ marginRight: '20px' }}
+                    >
+                        Save Changes
+                    </Button>
+                    <Button
+                        disabled={!this.state.isModified}
+                        onClick={this.cancel}
+                        variant="secondary"
+                    >
+                        Cancel Changes
+                    </Button>
                 </div>
             </div>
-            <div className="label relations">Relations:<input type="text" className="form-control" id="relationship"
-                onChange={e => this.change('relationship', e)} value={this.state.relationship} required />
-            </div>
-            <div className="label hobbies">Hobbies:<input type="text" className="form-control" id="hobbies"
-                onChange={e => this.change('hobbies', e)} value={this.state.hobbies} required />
-            </div>
-            <div className="label origin-city">City of origin:<input type="text" className="form-control" id="hobbies"
-                onChange={e => this.change('cityOfOrigin', e)} value={this.state.cityOfOrigin} required />
-            </div>
-            <div className="label city-of-living">Currently live in:<input type="text" className="form-control" id="hobbies"
-                onChange={e => this.change('cityOfLiving', e)} value={this.state.cityOfLiving} required />
-            </div>
-            <div className="label work-place">Work place:<input type="text" className="form-control" id="hobbies"
-                onChange={e => this.change('work', e)} value={this.state.work} required />
-            </div>
-            <div className="label education">Education:<input type="text" className="form-control" id="hobbies"
-                onChange={e => this.change('education', e)} value={this.state.education} required />
-            </div>
-        </div>
         );
     }
 
     showUser = () => {
         return (
-            <div>
-                <div className="birthday">Birthday: {this.state.dateOfBirth}</div>
-                <div className="email">E-mail: {this.state.email}</div>
-                <div className="email">Phone Number: {this.state.phoneNumber}</div>
-                <div className="email">Relations: {this.state.relationship}</div>
-                <div className="email">Interests: {this.state.hobbies}</div>
-                <div className="email">From: {this.state.cityOfOrigin}</div>
-                <div className="email">Living in: {this.state.cityOfLiving}</div>
-                <div className="email">Work: {this.state.work}</div>
-                <div className="email">Education: {this.state.education}</div>
-            </div>
+            <ListGroup variant="flush">
+                <ListGroup.Item>Birthday: {this.state.dateOfBirth}</ListGroup.Item>
+                <ListGroup.Item>E-mail: {this.state.email}</ListGroup.Item>
+                <ListGroup.Item>Phone Number: {this.state.phoneNumber}</ListGroup.Item>
+                <ListGroup.Item>Relations: {this.state.relationship}</ListGroup.Item>
+                <ListGroup.Item>Interests: {this.state.hobbies}</ListGroup.Item>
+                <ListGroup.Item>From: {this.state.cityOfOrigin}</ListGroup.Item>
+                <ListGroup.Item>Living in: {this.state.cityOfLiving}</ListGroup.Item>
+                <ListGroup.Item>Work: {this.state.work}</ListGroup.Item>
+                <ListGroup.Item>Education: {this.state.education}</ListGroup.Item>
+            </ListGroup>
         );
     }
 
